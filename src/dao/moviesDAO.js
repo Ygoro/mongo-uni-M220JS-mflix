1e7bbbc05a9c6a221d55e9c69cfc110a176b1e80
Igor Košmerl <i.kosmerl@gmail.com>
2020-07-09 12:41:38 +0200

    Ticket implementation - Faceted search
    Updated the moviesDAO,js facatedSearch method so that the application can perform faceted searches

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -193,12 +193,12 @@ export default class MoviesDAO {
     to complete this task, but you might have to do something about `const`.
     */
 
-    const queryPipeline = [
+    let queryPipeline = [
       matchStage,
       sortStage,
       // TODO Ticket: Faceted Search
       // Add the stages to queryPipeline in the correct order.
-    ]
+    ].concat([skipStage,limitStage,facetStage])
 
     try {
       const results = await (await movies.aggregate(queryPipeline)).next()
